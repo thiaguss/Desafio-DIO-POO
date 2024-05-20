b43f82edd930187f2628365aa035e66ca003f03b
Thiaguss <tsantos7098@gmail.com>
2024-05-20 19:03:53 -0300

    Commit final

---
+++ br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,68 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicio = LocalDate.now();
+    private final LocalDate dataFinal = dataInicio.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bootcamp bootcamp)) return false;
+        return Objects.equals(getNome(), bootcamp.getNome()) && Objects.equals(getDescricao(), bootcamp.getDescricao()) && Objects.equals(getDataInicio(), bootcamp.getDataInicio()) && Objects.equals(getDataFinal(), bootcamp.getDataFinal()) && Objects.equals(getDevsInscritos(), bootcamp.getDevsInscritos()) && Objects.equals(getConteudos(), bootcamp.getConteudos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getDescricao(), getDataInicio(), getDataFinal(), getDevsInscritos(), getConteudos());
+    }
+}
--- br/com/dio/desafio/dominio/Curso.java
+++ br/com/dio/desafio/dominio/Curso.java
@@ -1,27 +1,9 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
     private int cargaHoraria;
 
     public Curso() {}
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -31,12 +13,18 @@ public class Curso {
         this.cargaHoraria = cargaHoraria;
     }
 
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
     @Override
     public String toString() {
-        return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
+        return "Mentoria{" +
+                "carga horaria=" + cargaHoraria +
+                ", titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 '}';
     }
 }
